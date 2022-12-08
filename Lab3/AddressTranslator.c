de147da90b6ec39f6415960bf4e1e653a84891aa
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-12-08 23:55:29 +0100

    Lab 3 done

--- Lab3/AddressTranslator.c
+++ Lab3/AddressTranslator.c
@@ -25,22 +25,18 @@ To use in terminal:
 #define FRAME_SIZE 256
 #define TLB_SIZE 16
 
-
-
 /*
 The struct makes it easier to hold multiple data for each address.
 */
 struct address_components {
     int logical_address;
     int page_number;
+    int frame_number; // Actually redundant but useful during development
     int offset;
     int physical_address;
 };
 struct address_components addresses[FILE_SIZE];
 
-/*
-
-*/
 int page_table[PAGES];
 
 /* 
@@ -53,7 +49,7 @@ int pm_head;
 Holds up to 16 {page number, frame number} touples. tlb_head is a pointer for the TLB.
 */
 int tlb[TLB_SIZE][2];
-int tlb_head;
+int tlb_amount;
 
 /*
 The percentage of address references that resulted in page faults and the 
@@ -66,7 +62,7 @@ int tlb_hit_count;
 This function extracts the offset from a logical address.
 */
 int extractOffset(int logical_address) {
-    int offset = (logical_address) & 0xFF; 
+    int offset = logical_address & 0xFF;
     return offset;
 }
 
@@ -100,50 +96,69 @@ int readBin(int page_number) {
     fclose(bin_handle);
     return page_table[page_number];
 }
-/*
-int updateTLB() {
-    return 0;
-}
+
+/* 
+Updates the TLB with FIFO replacement strategy.
 */
-int consultTLB() {
-    //updateTLB();
-    return 0;
+void updateTLB(int page_number, int frame) {
+    if (tlb_amount == TLB_SIZE-1) {
+        for(int i = 0; i < TLB_SIZE-1; i++) {
+            tlb[i][0] = tlb[i+1][0];
+            tlb[i][1] = tlb[i+1][1];
+        }
+        tlb[TLB_SIZE-1][0] = page_number;
+        tlb[TLB_SIZE-1][1] = frame;
+        tlb_amount = 0;
+    }
+    else {
+        tlb[tlb_amount][0] = page_number;
+        tlb[tlb_amount][1] = frame;
+        tlb_amount++;
+    }
 }
 
+/*
+Searches the TLB (Cache) if the page exists and then returns the frame number.
+*/
+int searchTLB(int page_number) {
+    for(int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i][0] == page_number) {
+            return tlb[i][1];
+        } 
+    }
+    return -1;
+}
 
 
 /*
 Attempts to find the physical address with the TLB. If the address is not found then we consult
 the page table. In the case of page fault then we would read from the disk storage. 
 */
-int findPhysicalAddress(int page_number) {
-    int frame;
-
+int findPhysicalAddress(struct address_components address) {
+    int frame = searchTLB(address.page_number);
     // Check if in TLB WIP
-    if(consultTLB() != 0) {
-        //tlb_hit_count++;
-        //frame = tlb[]
+    if(frame != -1) {
+        tlb_hit_count++;
+        address.frame_number = frame;
     }
     else {
         // Check the page table
-        if (page_table[page_number] != -1) {
-            frame = page_table[page_number];
+        if (page_table[address.page_number] != -1) {
+            frame = page_table[address.page_number];
+            address.frame_number = frame;
         } 
         else {
             page_fault_count++;
             // Load in frame from bin
-            frame = readBin(page_number);
-            page_table[page_number] = frame;
+            frame = readBin(address.page_number);
+            page_table[address.page_number] = frame;
+            address.frame_number = frame;
         }
+        updateTLB(address.page_number, frame);
     }
     
-
-    // Concatenate frame number and offset to create the physical address
-    int physical_address = (frame << 8) & 0xFF00| addresses[page_number].offset & 0x00FF;
-    
-    // int temp = (frame << 8);
-    //printf("Frame: %d\n", temp);
-    printf("Physical address: %d\n", physical_address);
+    int physical_address = (frame << 8) + address.offset;
+    //printf("Virtual address: %d Physical address: %d Page Number: %d Frame: %d Offset: %d\n", address.logical_address, physical_address, address.page_number, frame, address.offset);
 
     return physical_address;
 }
@@ -161,36 +176,50 @@ int main(int argc, char *argv[]) {
     pm_head = 0;
     page_fault_count = 0;
     tlb_hit_count = 0;
-    tlb_head = 0;
+    tlb_amount = 0;
 
-    // Initialize the page table with "false" values 
+    // Initialize the page table and tlb with "false" values 
     for (int i = 0; i < 256; i++) {
         page_table[i] = -1;
+    }
+    for (int j = 0; j < 16; j++) {
+        tlb[j][0] = -1;
+        tlb[j][1] = -1;
     }
         
     // Parse the text file and call the extractors to fill our struct array
     char buffer[BUFFER_SIZE];
-    int i = 0;
+    int index = 0;
     int number; 
     while(fgets(buffer, BUFFER_SIZE - 1, file_handle) != NULL) {
         number = atoi(buffer);
-        addresses[i].logical_address = number;
-        addresses[i].page_number = extractPageNumber(number);
-        addresses[i].offset = extractOffset(number);
-        i++;
+        addresses[index].logical_address = number;
+        addresses[index].page_number = extractPageNumber(number);
+        addresses[index].offset = extractOffset(number);
+        index++;
     }
     fclose(file_handle);
 
+    // Debug
+    /*
+    for(int t = 0; t < FILE_SIZE; t++) {
+       printf("Virtual address: %d Page number: %d Offset: %d\n", addresses[t].logical_address, addresses[t].page_number, addresses[t].offset);
+    }
+    */
+
     // Start the physical address conversion process
-    for(int j = 0; j < FILE_SIZE; j++) {
-        addresses[j].physical_address = findPhysicalAddress(addresses[j].page_number);
+    for(int k = 0; k < FILE_SIZE; k++) {
+        addresses[k].physical_address = findPhysicalAddress(addresses[k]);
     }
 
     // Final print
-    for(int k = 0; k < FILE_SIZE; k++) {
-       //printf("Virtual address: %d Physical address: %d Value: TBD Offset: %d\n", addresses[k].logical_address, addresses[k].physical_address, addresses[k].offset);
+    for(int l = 0; l < FILE_SIZE; l++) {
+        // Using signed char we don't have to worry about flipping bits and sign extending
+        signed char value = physical_memory[page_table[addresses[l].page_number]][addresses[l].offset];
+        printf("Virtual address: %d Physical address: %d Value: %d\n", addresses[l].logical_address, addresses[l].physical_address, value);
     }
 
+    // Final statistics as the assignment requires.
     printf("Page-fault rate: %f\n", page_fault_count / 1000.0);
     printf("TLB hit rate: %f\n", tlb_hit_count / 1000.0);
 	return 0;
